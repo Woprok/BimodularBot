14bb4f07e45a79706b1d4523229f63eb9e7cfabd
Woprok <skvalach@gmail.com>
2025-03-22 11:23:04 +0100

    New Project

---
+++ Source/BimodularBot/BimodularBot.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BimodularBot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BimodularBot, "BimodularBot" );
