e4fe74e2e41b06ef7d8ff24b852d3d55de75f2a3
Woprok <skvalach@gmail.com>
2025-03-23 14:24:04 +0100

    Initial Setup

---
+++ Source/BimodularBot/Private/Core/BBActor.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/BBActor.h"
+
+// Sets default values
+ABBActor::ABBActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABBActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABBActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BimodularBot/Public/Core/BBGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BBGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIMODULARBOT_API ABBGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
